25b3f25592de21e7f49b6e3a40e5977264bc6c54
Thomas Kottke <t.kottke90@gmail.com>
2024-01-29 08:19:52 -0600

    refactor: Add create route fn

---
+++ src/errors/duplicate-route.error.ts
@@ -0,0 +1,3 @@
+export class DuplicateRouteError extends Error {
+  name = 'DuplicateRouteError';
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import { DuplicateRouteError } from './errors/duplicate-route.error';
 import { MissingRouteError } from './errors/missing-route.error';
 import { RouteEntry } from './route-entry';
 
 const Routes = new Map<string, RouteEntry>();
+
+const ROOT_ROUTE = new RouteEntry('/');
+Routes.set('ROOT', ROOT_ROUTE);
 
 export function getRoute(routeName: string) {
   const route = Routes.get(routeName);
@@ -10,4 +14,24 @@ export function getRoute(routeName: string) {
     throw new MissingRouteError(`Route [${routeName}] not registered`);
 
   return route;
+}
+
+export function createRoute(routeName: string, path: string, parent?: string) {
+  if (Routes.has(routeName)) {
+    throw new DuplicateRouteError(
+      `Route with name [${routeName}] already exists`
+    );
+  }
+
+  let parentRoute = ROOT_ROUTE;
+
+  if (parent) {
+    parentRoute = getRoute(parent);
+  }
+
+  const newRoute = new RouteEntry(path, parentRoute);
+
+  Routes.set(routeName, newRoute);
+
+  return newRoute;
 }
