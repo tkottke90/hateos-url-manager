1eed6283d53cf70d264ac69b733c6a00e81f8ecb
Thomas Kottke <t.kottke90@gmail.com>
2024-02-04 20:51:08 -0600

    refactor: Resolve combined url issue

--- src/route-entry.ts
+++ src/route-entry.ts
@@ -12,17 +12,17 @@ type ExtractParts<Path> = Path extends `${infer Segment}/${infer Rest}`
 // Combines 2 string literals delimited by a forward slash: users/:userId
 type MergedRoutes<
   ParentPath extends string,
-  ChildPath extends string
+  ChildPath
 > = ChildPath extends string ? `${ParentPath}/${ChildPath}` : never;
 
-let combined: MergedRoutes<'users', ':usersId'>;
+const combined: MergedRoutes<'users', ':usersId'> = 'users/:usersId';
 //    ^?
 
 export class Route<T extends string> {
   constructor(readonly path: T) {}
 
   createChild<ChildPath extends string>(path: ChildPath) {
-    const combinedPath: MergedRoutes<T, ChildPath> = `${this.path}/${path}`; // this.path + delimiter + path;
+    const combinedPath = `${this.path}/${path}` as MergedRoutes<T, ChildPath>; // this.path + delimiter + path;
 
     return new Route(combinedPath);
   }
--- test/demo.ts
+++ test/demo.ts
@@ -2,7 +2,10 @@ import { Route } from '../src/route-entry';
 
 const ROOT_ROUTE = new Route('');
 const USERS_ROUTE = ROOT_ROUTE.createChild('users');
-const USERID_ROUTE = new Route(':userId');
+const USERID_ROUTE = USERS_ROUTE.createChild(':userId');
+const USER_TRANSACTIONS_ROUTE = USERID_ROUTE.createChild(
+  'transactions/:transactionId'
+);
 
 ROOT_ROUTE.url({});
 //         ^?
@@ -10,3 +13,5 @@ USERS_ROUTE.url({});
 //          ^?
 USERID_ROUTE.url({ userId: '0' });
 //           ^?
+USER_TRANSACTIONS_ROUTE.url({ transactionId: '0', userId: '0' });
+//                      ^?
