8e4f06994d8ddfb32e585f4cad626f71b735dc7e
Thomas Kottke <t.kottke90@gmail.com>
2024-02-04 20:43:35 -0600

    refactor: Add notes to the route builder types

--- src/route-entry.ts
+++ src/route-entry.ts
@@ -1,11 +1,15 @@
+// Extract query parameters from a query string
 type ExtractParam<Path, NextPart> = Path extends `:${infer Param}`
   ? Record<Param, string> & NextPart
   : NextPart;
 
+// Parse each segment of a string (delimited by a forward slash) and return
+// a map of any query parameters
 type ExtractParts<Path> = Path extends `${infer Segment}/${infer Rest}`
   ? ExtractParam<Segment, ExtractParts<Rest>>
   : ExtractParam<Path, object>;
 
+// Combines 2 string literals delimited by a forward slash: users/:userId
 type MergedRoutes<
   ParentPath extends string,
   ChildPath extends string
