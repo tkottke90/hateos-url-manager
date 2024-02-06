6653749fc0c241981c7ab975d96ca37fdcaef136
Thomas Kottke <t.kottke90@gmail.com>
2024-02-06 13:42:53 -0600

    refactor: Add dist for installing

--- src/route-entry.ts
+++ src/route-entry.ts
@@ -1,30 +1,51 @@
-// Extract query parameters from a query string
-type ExtractParam<Path, NextPart> = Path extends `:${infer Param}`
-  ? Record<Param, string> & NextPart
-  : NextPart;
-
-// Parse each segment of a string (delimited by a forward slash) and return
-// a map of any query parameters
-type ExtractParts<Path> = Path extends `${infer Segment}/${infer Rest}`
-  ? ExtractParam<Segment, ExtractParts<Rest>>
-  : ExtractParam<Path, object>;
-
 // Combines 2 string literals delimited by a forward slash: users/:userId
 type MergedRoutes<
   ParentPath extends string,
   ChildPath
 > = ChildPath extends string ? `${ParentPath}/${ChildPath}` : never;
 
+type PathParams<T extends string> =
+  T extends `${string}:${infer Param}/${infer Rest}`
+    ? { [K in Param | keyof PathParams<Rest>]: string | number }
+    : T extends `${string}:${infer Param}`
+      ? { [K in Param]: string }
+      : Record<string, never>;
+
 export class Route<T extends string> {
-  constructor(readonly path: T) {}
+  private readonly _path: T;
+  private readonly _relativePath: string;
 
-  createChild<ChildPath extends string>(path: ChildPath) {
-    const combinedPath = `${this.path}/${path}` as MergedRoutes<T, ChildPath>;
-
-    return new Route(combinedPath);
+  constructor(path: T, relativePath?: string) {
+    this._path = path;
+    this._relativePath = relativePath ?? path;
   }
 
-  url(params: Record<keyof ExtractParts<T>, string>) {
-    return params;
+  get path(): string {
+    return `/${this._path}`;
+  }
+
+  get relativePath(): string {
+    return `/${this._relativePath}`;
+  }
+
+  createChild<ChildPath extends string>(path: ChildPath) {
+    const combinedPath = `${this._path}/${path}` as MergedRoutes<T, ChildPath>;
+
+    return new Route(combinedPath, path);
+  }
+
+  url<Params extends PathParams<T> = PathParams<T>>(
+    params?: keyof Params extends never ? undefined : Params
+  ): string {
+    let output = this._path as string;
+
+    if (!params) return output;
+
+    for (const key of Object.keys(params ?? {})) {
+      const value = params[key];
+      output = output.replace(`:${key}`, value);
+    }
+
+    return output;
   }
 }
